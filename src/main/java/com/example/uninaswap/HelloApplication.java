bcc46cc95ba25eab2152c5645688fe139083a193
fire <francesco04_2@outlook.it>
2025-10-07 18:36:35 +0200

    Creazione della Navbar e del suo controller

--- src/main/java/com/example/uninaswap/HelloApplication.java
+++ src/main/java/com/example/uninaswap/HelloApplication.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("navbar.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
