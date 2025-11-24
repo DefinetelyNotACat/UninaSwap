affa38dfcca4d5d7b3f1373dbe5ab1bd5a0ec6bd
DefinetelyNotACat <anacleriapaky@gmail.com>
2025-11-25 00:10:58 +0100

    Entità Offerta e Annuncio inserite

--- src/main/java/entity/AnnuncioVendita.java
+++ src/main/java/entity/AnnuncioVendita.java
@@ -1,4 +1,9 @@
 package entity;
 
+import java.time.LocalTime;
+
 public class AnnuncioVendita extends Annuncio{
+    public AnnuncioVendita(Sede sede, String descrizione, LocalTime orarioInizio, LocalTime orarioFine, Oggetto oggetto) {
+        super(sede, descrizione, orarioInizio, orarioFine, oggetto);
+    }
 }
--- src/main/java/entity/OffertaVendita.java
+++ src/main/java/entity/OffertaVendita.java
@@ -1,4 +1,9 @@
 package entity;
 
+import java.time.LocalTime;
+
 public class OffertaVendita extends Offerta{
+    public OffertaVendita(String messaggio, STATO_OFFERTA stato, LocalTime orarioInizio, LocalTime orarioFine, Oggetto oggetto) {
+        super(messaggio, stato, orarioInizio, orarioFine, oggetto);
+    }
 }
