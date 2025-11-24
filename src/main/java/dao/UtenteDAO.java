46ca92b1d82f6b480e08e13be55b29e3d3a4bb08
Fire <francesco04_2@outlook.it>
2025-11-24 01:55:11 +0100

    Definizione insert della classe UtenteDAO.java

--- src/main/java/dao/UtenteDAO.java
+++ src/main/java/dao/UtenteDAO.java
@@ -6,7 +6,23 @@ import java.util.List;
 import entity.*;
 
 public class UtenteDAO {
-    public boolean salvaUtente(Utente utente){return true;}
+    public boolean salvaUtente(Utente utente){
+        String sql = "INSERT INTO utente (username, password, matricola, email) VALUES (?, ?, ?, ?)";
+        try (Connection connessione = PostgreSQLConnection.getConnection();
+             PreparedStatement query = connessione.prepareStatement(sql)){
+            query.setString(1, utente.getUsername());
+            query.setString(2, utente.getPassword());
+            query.setString(3, utente.getMatricola());
+            query.setString(4, utente.getEmail());
+
+            int numModifiche = query.executeUpdate();
+            return numModifiche > 0;
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
     public boolean modificaUtente(Utente utente){return true;}
     public boolean eliminaUtente(Utente utente){return true;}
     public Utente ottieniUtente(int id){return null;}
