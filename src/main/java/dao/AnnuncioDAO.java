b29824a72d671057045bc9158f24a76c7a6cd268
Fire <francesco04_2@outlook.it>
2025-11-24 18:29:29 +0100

    Creazione interfaccie e popolazione di alcune

--- src/main/java/dao/AnnuncioDAO.java
+++ src/main/java/dao/AnnuncioDAO.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import entity.*;
 
 public class AnnuncioDAO {
-
     public boolean salvaAnnuncio(Annuncio annuncio){return true;}
     public boolean modificaAnnuncio(Annuncio annuncio){return true;}
     public boolean salvaAnnuncio(int id){return true;}
