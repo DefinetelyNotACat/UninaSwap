2de31526658390ff52b819317ef2fed2c4169447
fire <francesco04_2@outlook.it>
2025-12-17 14:16:09 +0100

    Bug Fixies

--- src/main/java/com/example/uninaswap/interfaces/GestoreOggettoCategoriaDAO.java
+++ src/main/java/com/example/uninaswap/interfaces/GestoreOggettoCategoriaDAO.java
@@ -3,12 +3,13 @@ package com.example.uninaswap.interfaces;
 import com.example.uninaswap.entity.Categoria;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface GestoreOggettoCategoriaDAO {
     public boolean associaCategoria(int idOggetto, String nomeCategoria);
     public boolean rimuoviAssociazione(int idOggetto, String nomeCategoria);
-    public boolean rimuoviTutteLeCategorieDiOggetto(int idOggetto, Connection connEsterna);
+    public boolean rimuoviTutteLeCategorieDiOggetto(int idOggetto, Connection connEsterna) throws SQLException;
     public ArrayList<Categoria> ottieniCategoriePerOggetto(int idOggetto);
-    public void salvaListaCategorie(Connection conn, int idOggetto, ArrayList<Categoria> categorie);
+    public void salvaListaCategorie(Connection conn, int idOggetto, ArrayList<Categoria> categorie) throws SQLException;
 }
