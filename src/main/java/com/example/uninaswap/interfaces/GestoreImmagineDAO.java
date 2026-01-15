6843aacf3cd2179ed8217fe891f8d946ad2e98c3
Gaetano Borriello <gaetano.borriello@gmail.com>
2026-01-15 17:32:52 +0100

    bug fixes

--- src/main/java/com/example/uninaswap/interfaces/GestoreImmagineDAO.java
+++ src/main/java/com/example/uninaswap/interfaces/GestoreImmagineDAO.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface GestoreImmagineDAO {
+    public boolean rimuoviImmagine(Immagine immagine);
+
     public boolean inserisciImmagine(String immagine, int idOggetto);
     public boolean inserisciImmagine(Connection conn, String immagine, int idOggetto) throws SQLException;
     public boolean rimuoviImmagine(Connection conn, int idOggetto) throws SQLException;
