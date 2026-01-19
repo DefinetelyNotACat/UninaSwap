a1e0c2d86e4e52fa8622030a1d9166396eff5a0f
PixelHashi <anacleriapaky@gmail.com>
2026-01-19 01:44:51 +0100

    ottimizzazioni

--- src/main/java/com/example/uninaswap/Costanti.java
+++ src/main/java/com/example/uninaswap/Costanti.java
@@ -14,25 +14,19 @@ public class Costanti {
     public static final String pathUtenti = "dati_utenti";
     public static final String pathInventario = "/com/example/uninaswap/inventario.fxml";
     public static final String pathAggiungiOggetto = "/com/example/uninaswap/aggiungiOggetto.fxml";
-    public static final String aggiungiOggetto = "aggiungi oggetto";
     public static final String EMAIL_REGEX_UNINA = "^[a-zA-Z0-9.]{6,64}@studenti\\.unina\\.it$";
     public static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9@$!%*?&._-]{8,25}$";
     public static final String FIELDS_REGEX = "^[a-zA-Z0-9]+$";
     public static final String FIELDS_REGEX_SPAZIO = "^[a-zA-Z0-9 ?!\\\"'€]+$";
     public static final String ALMENO_UN_NUMERO_REGEX = ".*\\d.*";
-    public static final String OGGETTO_FIELD_REGEX = "^[a-zA-Z0-9 ]+$";
     public static final String PRICE_REGEX = "^[0-9]+([.,][0-9]{1,2})?$";
-    public static final String TIME_REGEX = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$";
-    public static final String inventario = "il mio inventario";
     public static final String pathCreaAnnuncio = "/com/example/uninaswap/aggiungiAnnuncio.fxml";
     public static final String NOME_CARTELLA_IMMAGINI_PFP = "immagini";
-    public static final String BASE_PATH = "/com/example/uninaswap/";
     public static final String pathGestioneOfferte = "/com/example/uninaswap/gestioneOfferte.fxml";
     public static final String pathMieiAnnunci = "/com/example/uninaswap/mieiAnnunci.fxml";
     public static final String pathDettaglioAnnuncio = "/com/example/uninaswap/dettaglioAnnuncio.fxml";
     public static final String pathEffettuaOfferta = "/com/example/uninaswap/effettuaOfferta.fxml";
     public static final String pathRecensioni = "/com/example/uninaswap/recensioni.fxml";
     public static final String pathAggiungiRecensione = "/com/example/uninaswap/aggiungiRecensione.fxml";
-    public static final String pathReportOfferte = "/com/example/uninaswap/reportOfferte.fxml";
     public static final String pathNessunaOfferta = "/com/example/uninaswap/nessunaOfferta.fxml";
 }
